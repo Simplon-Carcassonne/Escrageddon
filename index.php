dcc193bd250c95fd7df8f7f7499f3661b7e8fbdd
Kellian1998 <kellianchamouardcours@gmail.com>
2020-07-31 15:40:15 +0200

    add snail div

--- index.php
+++ index.php
@@ -14,7 +14,13 @@
 
         <div id="logo"><img src="images/logescar.png" alt="Logo Escargeddon"></div>
 
-        <div class="racer_table"></div>
+        <div class="racer_table">
+            <div id="snail1"></div>
+            <div id="snail2"></div>
+            <div id="snail3"></div>
+            <div id="snail4"></div>
+            <div id="snail5"></div>
+        </div>
 
         <script src="js/Escargot.js"></script>
         <script src="js/Game.js"></script>
