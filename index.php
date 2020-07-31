fbedc9d14e3f94ae5c639bc481325092ccf0607a
Kellian Chamouard <kellianchamouard@MacBook-Pro-de-Kellian.local>
2020-07-31 15:28:34 +0200

    add index logo css

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style/style.css">
+        <link
+            href="https://fonts.googleapis.com/css?family=Jockey+One&display=swap"
+            rel="stylesheet">
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <title>Escargeddon</title>
+    </head>
+    <body>
+
+        <div id="logo"><img src="images/logescar.png" alt="Logo Escargeddon"></div>
+
+        <div class="racer_table"></div>
+
+        <script src="js/Escargot.js"></script>
+        <script src="js/Game.js"></script>
+        <script src="js/app.js"></script>
+    </body>
+</html>
