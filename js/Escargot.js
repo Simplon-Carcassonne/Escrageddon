b26290146e1196b824d7c6249a88dab539ab3e44
Yirzaraji <not.iremy@gmail.com>
2020-07-31 14:55:16 +0200

    add escargot object

---
+++ js/Escargot.js
@@ -0,0 +1,18 @@
+class Escargot {
+
+    constructor(id, life, image, abilities = null) {
+        this.id = id
+        this.life = life
+        this.image = image
+        this.abilities = abilities
+
+    }
+
+    snailMove() {
+
+    }
+
+    snailAttack() {
+
+    }
+}
